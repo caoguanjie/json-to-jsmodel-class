968b1ba02d639eb39c37b8f51b5aed95eb8543b5
caoguanjie <374348532@qq.com>
2023-08-07 16:07:17 +0800

    ts的基本类型要小写

--- src/helpers/stringHelper.ts
+++ src/helpers/stringHelper.ts
@@ -1,3 +1,9 @@
-export const capitalize = (word: String) => word[0].toUpperCase() + word.slice(1); 
+export const capitalize = (word: string) => {
+    const baseType = ['any', 'boolean', 'number', 'string', 'array', 'void', 'Undefined', 'null', 'never', 'unknown'];
+    if (baseType.includes(word)) {
+        return word;
+    }
+    return word[0].toUpperCase() + word.slice(1);
+};
 
 export const isDateTimeISO = (word: string) => /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/.test(word);
